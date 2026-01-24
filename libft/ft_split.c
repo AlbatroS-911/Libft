da4e0589b9430673b63323f22bffbb0236079dd0
Toky Erick Rabemanantsoa <tokrabem@c1r9s15.42antananarivo.mg>
2026-01-24 08:37:25 +0300

    libft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,11 +6,12 @@
 /*   By: tokrabem <tokrabem@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 21:27:36 by tokrabem          #+#    #+#             */
-/*   Updated: 2026/01/24 03:11:20 by tokrabem         ###   ########.fr       */
+/*   Updated: 2026/01/24 08:36:52 by tokrabem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stdlib.h"
+#include "libft.h"
 
 char	**ft_split(char const *s, char c)
 {
@@ -19,7 +20,7 @@ char	**ft_split(char const *s, char c)
 	int	j;
 	int k;
 
-	splitted = (char **) malloc(sizeof(char *) );
+	splitted = (char **) malloc(sizeof(char *) * ft_strlen(s));
 	i = 0;
 	k= 0;
 	if (!splitted)
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: tokrabem <tokrabem@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 19:24:37 by tokrabem          #+#    #+#             */
-/*   Updated: 2026/01/24 03:08:18 by tokrabem         ###   ########.fr       */
+/*   Updated: 2026/01/24 08:34:20 by tokrabem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int main()
 {
-	char s[] = "hey ciao my bud civil monkey. I can see your can inside my closet";
+	char s[] = "hey congrat's my bud civil monkey. I can see your can inside my closet";
 	char **res;
 	int i;
 
